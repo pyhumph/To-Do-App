b9aa2d664a45ea2a686c2b23f211fcdef3a37222
pyhumph <humphrey.david.dev@gmail.com>
2024-03-15 12:42:04 +0300

    add javascript listener function on task list

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ function addTask(){
     }
     boxContainer.value = "";
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+
